2832ddfd3ffb61cdc78f2ca583b03b8db6d0d008
Gavin Wong <gavinmwong@gmail.com>
2019-12-30 21:03:13 -0800

    day 5 problem 6

---
+++ src/CowAtLarge.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class CowAtLarge {
+
+	private static int N;
+	private static int K;
+	public static void main(String[] args) throws Exception{
+		Scanner in = new Scanner(System.in);
+		N = in.nextInt();
+		K = in.nextInt();
+		
+		
+
+	}
+
+}
